395d38b2c947e0cefbb8e8b0da0e10fe7c463c28
Carlo Lomod <30666042+superloika@users.noreply.github.com>
2019-06-02 20:07:40 +0800

    Add files via upload

---
+++ js/superloika.js
@@ -0,0 +1,8 @@
+var top_social = document.getElementById('top_social');
+var bottom_social = document.getElementById('bottom_social');
+
+var top_nav = document.getElementById('top_nav');
+var bottom_nav = document.getElementById('bottom_nav');
+
+bottom_social.innerHTML = top_social.innerHTML;
+bottom_nav.innerHTML = top_nav.innerHTML;
