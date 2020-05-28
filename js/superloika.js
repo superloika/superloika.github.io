7a079169700b0577315f0c2322a149773b27b471
superloika <superloika25@gmail.com>
2020-05-28 15:44:29 +0800

    Minimal color modifications

--- js/superloika.js
+++ js/superloika.js
@@ -6,3 +6,8 @@ var bottom_nav = document.getElementById('bottom_nav');
 
 bottom_social.innerHTML = top_social.innerHTML;
 bottom_nav.innerHTML = top_nav.innerHTML;
+
+// May 28, 2020
+let el_year = document.getElementById('year');
+let curYear = new Date();
+el_year.innerText = curYear.getFullYear();
